bd41e432704070bbba7c53f720974a05dedd4f9f
Chris <Djchris114211@gmail.com>
2024-09-23 11:56:51 -0400

    Finished Product

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,27 @@ import rawTriviaQuestion from "./lib/data";
 
 const triviaQuestion = rawTriviaQuestion.results[0];
 
+const URL = "https://opentdb.com/api.php?amount=1&category=9&type=multiple";
+
 function App() {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [questionData, setQuestionData] = useState(triviaQuestion);
 
   const selectAnswer = (selection) => {
     setSelectedAnswer(selection);
+  };
+
+  // It is having that rate limit issue, where if you click next question too fast it wont go to the next question but rather reload this question.
+
+  const fetchQuestion = async () => {
+    setQuestionData(null);
+    const response = await fetch(URL);
+    const data = await response.json();
+    setQuestionData(data.results[0]);
+  };
+
+  const handleNextQuestion = () => {
+    fetchQuestion();
   };
 
   let card;
@@ -41,7 +56,9 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={handleNextQuestion}>
+          Next Question
+        </button>
         {card}
       </div>
     </div>
