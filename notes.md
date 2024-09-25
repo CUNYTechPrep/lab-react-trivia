5a0e2a79991639d6afd68c87d97e18317702bd73
Chris <Djchris114211@gmail.com>
2024-09-25 17:42:47 -0400

    finalized commit

--- notes.md
+++
@@ -1,7 +0,0 @@
-This is notes for what I need to do for the lab
-
-Add an event handler for the Next Question button - the answerbutton.jsx file
-
-Fetch a new question for the app when the user clicks the Next Question button
-use this API URL: https://opentdb.com/api.php?amount=1&category=9&type=multiple
-the API will return a random question with each call
--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import rawTriviaQuestion from "./lib/data";
 
 const triviaQuestion = rawTriviaQuestion.results[0];
 
-const URL = "https://opentdb.com/api.php?amount=1&category=9&type=multiple";
-
 function App() {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [questionData, setQuestionData] = useState(triviaQuestion);
@@ -16,16 +14,19 @@ function App() {
     setSelectedAnswer(selection);
   };
 
-  // It is having that rate limit issue, where if you click next question too fast it wont go to the next question but rather reload this question.
+  // It is having that rate limit issue, where if you click next question too fast
+  // it wont go to the next question but rather reload this question.
 
   const fetchQuestion = async () => {
-    setQuestionData(null);
-    const response = await fetch(URL);
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=1&category=9&type=multiple"
+    );
     const data = await response.json();
     setQuestionData(data.results[0]);
   };
 
   const handleNextQuestion = () => {
+    setSelectedAnswer(null);
     fetchQuestion();
   };
 
