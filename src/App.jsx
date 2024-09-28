35e67a2b6719d25e03df0445201ade6a1b51f96a
justinyc1 <justinyijiachen@gmail.com>
2024-09-27 20:50:40 -0400

    added event handler and trivia DB API fetching

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,23 @@ function App() {
     setSelectedAnswer(selection);
   };
 
+  function getNextQuestion() {
+    fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple")
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        // } else if (response.status === 429) { // not a part of assignment but I thought was nice to include
+        //   alert("You are submitting qanswers too fast!");
+        } else {
+          console.log(response.status);
+        }
+      })
+      .then((data) => {
+        setQuestionData(data.results[0]);
+        setSelectedAnswer(false);
+      })
+  }
+
   let card;
 
   if (selectedAnswer) {
@@ -41,7 +58,7 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={() => getNextQuestion()}>Next Question</button>
         {card}
       </div>
     </div>
